f2cb97ccc05fdf4510c936449cd5af0db7c0fdc8
Rod Andrade <rodrigoandradebcc@gmail.com>
2016-06-08 01:21:07 -0300

    Tentativa de relacionamento

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,10 +1,9 @@
 class Account < ActiveRecord::Base
   belongs_to :chart_of_account
   has_many :group
-  
+
   has_and_belongs_to_many :operations
 
   validates_presence_of :code, :name, :description, :chart_of_account_id
-
 
 end
