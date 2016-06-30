ccae5835cc86c3e03dee0fdd62e355922a999b2a
akmerejf <akmerejf@gmail.com>
2016-06-30 18:20:45 -0300

    ajustes nas migrations

--- db/migrate/20160628205141_create_operations.rb
+++ db/migrate/20160628205141_create_operations.rb
@@ -2,14 +2,8 @@ class CreateOperations < ActiveRecord::Migration
   def change
     create_table :operations do |t|
       t.float :value
-<<<<<<< HEAD:db/migrate/20160628205141_create_operations.rb
       t.text :description
-      t.date :launch_date
-=======
-      t.string :description
-      t.string :release_date
->>>>>>> 8fcd3ff446731673164eaabacc396d576e15daef:db/migrate/20160605141631_create_operations.rb
-
+      t.date :release_date
       t.timestamps null: false
     end
   end
