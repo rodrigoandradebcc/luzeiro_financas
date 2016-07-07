7ea82ba5cf49c40d81545aaa5291a4ab9176c21a
Rodrigo Andrade <rodrigoandradebcc@gmail.com>
2016-07-07 00:16:06 -0300

    Design

--- app/controllers/analytic_accounts_controller.rb
+++ app/controllers/analytic_accounts_controller.rb
@@ -8,7 +8,7 @@ class AnalyticAccountsController < ApplicationController
   end
 
   def analytic_ledger
-    @operatons = Operation.where()
+    @operations = Operation.includes(:release_account).where(release_account: @analytic_account)
   end
 
   # GET /analytic_accounts/1
