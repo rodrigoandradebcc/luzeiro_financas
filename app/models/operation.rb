a35a2acf3ccc95e406d1aeb6733f010344769e8e
akmerejf <akmerejf@gmail.com>
2016-09-30 11:54:38 -0300

    validação de negócio - contas não podem ser iguais no select box de operações

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -13,11 +13,15 @@ class Operation < ActiveRecord::Base
   # after_update :update_balance
   
 
-  validates :value, :release_date, presence: true
+  validates :value, :only_one_account_per_select_box, :release_date, presence: true
+
+  def only_one_account_per_select_box
+    unless retrieve_account != release_account
+      errors.add("Contas iguais:", "As contas devem ser diferentes umas das outras")
+    end
+  end
 
 
-
-  
   def self.id_search(query)
     where("id like ?", "%#{query}%") 
   end
